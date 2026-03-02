792d6394a42f2ac87bfed30ff528303782fa166f
jameelcholakkal2025-hue <jameelcholakkal2025@gmail.com>
2026-03-02 13:56:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post("/legal-ai", async (req, res) => {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
